af13e1a2889bd8e0651e711b22f005e00f0b3df9
Blerim Cazimi <blerim@burd.dk>
2020-08-06 15:11:53 +0200

    Recurring price

--- src/Models/SubscriptionOrderUpdate.php
+++ src/Models/SubscriptionOrderUpdate.php
@@ -34,12 +34,19 @@ class SubscriptionOrderUpdate
     public int $billing_model_id;
 
 
-    public function __construct(int $order_id, int $product_id, string $new_recurring_product_id, int $billing_model_id)
+    /**
+     * @OA\Property(type="number", example="19.99")
+     */
+    public float $new_recurring_price;
+
+
+    public function __construct(int $order_id, int $product_id, string $new_recurring_product_id, int $billing_model_id, float $new_recurring_price)
     {
         $this->order_id = $order_id;
         $this->product_id = $product_id;
         $this->new_recurring_product_id = $new_recurring_product_id;
         $this->billing_model_id = $billing_model_id;
+        $this->new_recurring_price = $new_recurring_price;
     }
 
 }
