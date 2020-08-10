1472cbac06cc0d8a70d628a48b0121db96cc7667
Blerim Cazimi <blerim@burd.dk>
2020-08-10 13:25:45 +0200

    composer.json src

--- src/Models/OrderExistsOrderFlow.php
+++ src/Models/OrderExistsOrderFlow.php
@@ -25,4 +25,15 @@ class OrderExistsOrderFlow
      */
     public int $flow_id;
 
+    /**
+     * OrderExistsOrderFlow constructor.
+     * @param int $customer_id
+     * @param int $flow_id
+     */
+    public function __construct(int $customer_id, int $flow_id)
+    {
+        $this->customer_id = $customer_id;
+        $this->flow_id = $flow_id;
+    }
+
 }
